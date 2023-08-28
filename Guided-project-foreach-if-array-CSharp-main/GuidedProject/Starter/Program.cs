f650f7d270712f3f85f045bdd1b3f4196ee71133
gusta <gustavoandres_r@hotmail.com>
2023-08-28 19:45:54 +0200

    Primera iteración foreach

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -8,64 +8,34 @@ using System;
 // initialize variables - graded assignments 
 int currentAssignments = 5;
 
-int sophia1 = 90;
-int sophia2 = 86;
-int sophia3 = 87;
-int sophia4 = 98;
-int sophia5 = 100;
+// Creamos las matrices para las puntuaciones.
 
-int andrew1 = 92;
-int andrew2 = 89;
-int andrew3 = 81;
-int andrew4 = 96;
-int andrew5 = 90;
-
-int emma1 = 90;
-int emma2 = 85;
-int emma3 = 87;
-int emma4 = 98;
-int emma5 = 68;
-
-int logan1 = 90;
-int logan2 = 95;
-int logan3 = 87;
-int logan4 = 88;
-int logan5 = 96;
-
-//  enteros que se usan para mantener la suma de las puntuaciones del examen.
+int[] sopiaScores = new int [] {90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] {92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] {90, 85, 87, 98, 68 };
+int[] loganScores = new int[] {90, 95, 87, 88, 96 };
 
 int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
-
-// decimales que se usan para mantener la puntuación media calculada.
 
 decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
 
-// calcular la suma de las puntuaciones de tareas de cada alumno
+/*
+Creamos una instancia de una variable entera denominada "score" como parte 
+de la instrucción foreach. Usaremos score dentro del bloque de código de foreach 
+para acceder a los valores de la matriz sophiaScores.
+*/
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
-
-// calcula la puntuación media.
+foreach (int score in sophiaScore)
+{
+    // add the exam score to the sum
+    sophiaSum += score; // usamos el operador += para agregar el valor de score 
+                        // a sophiaSum dentro del bucle foreach. Equivale a sophiaSum = sophiaSum + score;
+}
 
 sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
 
 Console.WriteLine("Student\t\tGrade\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine(); // detiene la aplicación para que el usuario pueda examinar la salida.
- 
