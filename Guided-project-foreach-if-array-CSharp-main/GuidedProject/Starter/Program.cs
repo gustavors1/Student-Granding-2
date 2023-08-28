bf4205d3984fb7200177dca758ff8ed3a2d0355a
gusta <gustavoandres_r@hotmail.com>
2023-08-28 20:07:24 +0200

    Anidación de los bucles

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -8,34 +8,50 @@ using System;
 // initialize variables - graded assignments 
 int currentAssignments = 5;
 
-// Creamos las matrices para las puntuaciones.
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int[] sopiaScores = new int [] {90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] {92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] {90, 85, 87, 98, 68 };
-int[] loganScores = new int[] {90, 95, 87, 88, 96 };
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
-int sophiaSum = 0;
+int[] studentScores = new int[10];
 
-decimal sophiaScore;
-
-/*
-Creamos una instancia de una variable entera denominada "score" como parte 
-de la instrucción foreach. Usaremos score dentro del bloque de código de foreach 
-para acceder a los valores de la matriz sophiaScores.
-*/
-
-foreach (int score in sophiaScore)
-{
-    // add the exam score to the sum
-    sophiaSum += score; // usamos el operador += para agregar el valor de score 
-                        // a sophiaSum dentro del bucle foreach. Equivale a sophiaSum = sophiaSum + score;
-}
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-
+// Write the Report Header to the console
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+}
 Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine(); // detiene la aplicación para que el usuario pueda examinar la salida.
+Console.ReadLine();
